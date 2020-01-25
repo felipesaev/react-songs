d6057b2f3c71be7fa880f6200b6076b27780ab70
Felipe <felipesaev@gmail.com>
2020-01-24 21:14:31 -0300

    adicionando um contexto de autenticação

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Login  from './components/Login'
+import Header  from './components/Header'
+import Home  from './components/Home'
+
+export const AuthContext = React.createContext(); // added this
+
 function App() {
 return (
-      <div className="App"></div>
+    <AuthContext.Provider>
+            <div className="App"></div>
+     </AuthContext.Provider>
+
   );
 }
 export default App;
